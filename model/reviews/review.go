253da4875cba3638ddbeeaab227b0c46819e4383
eto-shingo <=>
2019-03-18 00:51:46 +0000

    test: use GetAll() instead of count and Run() Next().

--- model/reviews/review.go
+++ model/reviews/review.go
@@ -60,30 +60,41 @@ func IncrementIine(ctx context.Context, id int64) error {
 	return nil
 }
 func GetUserReviews(ctx context.Context, userId int64) (map[int64]*Review, error) {
-	var ret map[int64]*Review
 
-	// SELECT * FROM Reviews WHERE UserID = 'userId' ORDER BY createdAt
+	reviews := []Review{}
 	query := datastore.NewQuery(Kind).Filter("UserID =", userId)
-	count, err := query.Count(ctx)
+	keys, err := query.GetAll(ctx, &reviews)
 	if err != nil {
-		return ret, errors.New(fmt.Sprintf("unable query count  %s", err))
+		return nil, errors.New(fmt.Sprintf("unable datastore %s", err))
 	}
 
-	// do not use GetALL. Because it has 1000 limit.
-	ret = make(map[int64]*Review, count)
-	itr := query.Run(ctx)
-
-	for {
-		var review Review
-		key, err := itr.Next(&review)
-		if err == datastore.Done {
-			break
-		}
+	/*
+		count, err := query.Count(ctx)
 		if err != nil {
-			return ret, errors.New(fmt.Sprintf("unable datastore %s", err))
+			return ret, errors.New(fmt.Sprintf("unable query count  %s", err))
 		}
 
-		review.ID = key.IntID()
+		// do not use GetALL. Because it has 1000 limit.
+		ret = make(map[int64]*Review, count)
+		itr := query.Run(ctx)
+
+		for {
+			var review Review
+			key, err := itr.Next(&review)
+			if err == datastore.Done {
+				break
+			}
+			if err != nil {
+				return ret, errors.New(fmt.Sprintf("unable datastore %s", err))
+			}
+
+			review.ID = key.IntID()
+			ret[review.ID] = &review
+		}
+	*/
+	ret := make(map[int64]*Review, len(reviews))
+	for i, review := range reviews {
+		review.ID = keys[i].IntID()
 		ret[review.ID] = &review
 	}
 
